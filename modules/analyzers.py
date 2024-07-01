b2992d457b8aab40007e9a11c795bfa4ec209a29
urerc <urerc@student.kit.edu>
2024-07-01 10:49:37 +0200

    fixed error in tag_peaks

--- modules/analyzers.py
+++ modules/analyzers.py
@@ -1,11 +1,11 @@
 from scipy import signal
 
-def tag_peaks(input_data, prominence, distance, width):
+def tag_peaks(input_data, peak_config):
     peaks = {}
     peaks_prop = {}
     for key in input_data.dtype.names:
         peaks[key], peaks_prop[key] = signal.find_peaks(
-            input_data[key], prominence=prominence, distance=distance, width=width
+            input_data[key], prominence=peak_config['prominence'], distance=peak_config['distance'], width=peak_config['width']
         )
     return peaks, peaks_prop
 
