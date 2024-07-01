633e31b422ce89e7dbfe894053f5525ee7954546
Guenter Quast <guenter.quast@online.de>
2024-07-01 19:16:38 +0200

    more flexible pulse geneator: multiple pulse heights

--- modules/pulseSimulator.py
+++ modules/pulseSimulator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 
+
 class pulseSimulator:
     """generate wavevorm data of typical pulses from particle detectors, 
        characterised by an exponential shape with parameters height and 
@@ -22,13 +23,16 @@ class pulseSimulator:
             config_dict["pulseWindow"]
         self.pulse_height = 250.0 if "pulseHeight" not in config_dict else\
             config_dict["pulseHeight"]
+        if not isinstance(self.pulse_height, list):
+            self.pulse_height = [self.pulse_height]
+        self.pulse_height = np.array(self.pulse_height)
         self.pulse_spread = self.pulse_height * 0.3 if "pulseSpread" not in config_dict else\
             config_dict["pulseSpread"]
         self.tau = self.plen / 4.0  # decay time of exponential pulse
         self.mn_position = self.pre_trigger_samples
         self.mx_position = self.number_of_samples - self.plen
         self.pulse_template = np.exp(-np.float32(np.linspace(0.0, self.plen, self.plen, endpoint=False)) / self.tau)
-        self.noise = self.pulse_height / 30.0
+        self.noise = self.pulse_height.mean() / 30.0
         self.tau_mu = 2197  # muyon life time in ns
         self.detector_efficiency = 0.95 if "prbInteraction" not in config_dict else\
             config_dict["prbInteraction"]
@@ -49,11 +53,11 @@ class pulseSimulator:
         # one pulse at trigger position in layers one and two
         for i_layer in range(n1):
             # random pulse height for trigger pulse
-            pheight = self.pulse_height + self.pulse_spread * np.random.normal()
+            pheight = np.random.choice(self.pulse_height) + self.pulse_spread * np.random.normal()
             if i_layer == 0:
                 #  respect trigger condition in layer 1
                 while pheight < self.trigger_level:
-                    pheight = self.pulse_height + self.pulse_spread * np.random.normal()
+                    pheight =np.random.choice(self.pulse_height) + self.pulse_spread * np.random.normal()
             if np.random.rand() < self.detector_efficiency:
                 pulse[i_layer, self.mn_position : self.mn_position + self.plen] += pheight * self.pulse_template
 
@@ -66,14 +70,14 @@ class pulseSimulator:
                 for i_layer in range(0, min(nchan, 2)):
                     # random pulse height and position for 2nd pulse
                     ## pheight2 = np.random.rand()*maxheight
-                    pheight2 = self.pulse_height + self.pulse_spread * np.random.normal()
+                    pheight2 = np.random.choice(self.pulse_height) + self.pulse_spread * np.random.normal()
                     if np.random.rand() < self.detector_efficiency and pos2 < self.mx_position:
                         pulse[i_layer, pos2 : pos2 + self.plen] += pheight2 * self.pulse_template
             else:
                 for i_layer in range(min(nchan, 2), min(nchan, 4)):
                     # random pulse height and position for 2nd pulse
                     ## pheight2 = np.random.rand()*maxheight
-                    pheight2 = self.pulse_height + self.pulse_spread * np.random.normal()
+                    pheight2 = np.random.choice(self.pulse_height) + self.pulse_spread * np.random.normal()
                     if np.random.rand() < self.detector_efficiency and pos2 < self.mx_position:
                         pulse[i_layer, pos2 : pos2 + self.plen] += pheight2 * self.pulse_template
         pulse += self.analogue_offset_mv  # apply analogue offset
